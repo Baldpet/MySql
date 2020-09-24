46ee345263a9a90acf17658b750ca47431de229d
Baldpet <peterdaniel.baldwin@gmail.com>
2020-09-24 16:33:08 +0000

    adding last bits

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,20 +1,21 @@
 import os
+import datetime
 import pymysql
 
-#Get username from Gitpod
+# Get username from Gitpod
 
 username = os.getenv('Gitpod_User')
 
 # Connect to the database
-connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
+connection = pymysql.connect(
+    host='localhost', user=username, password='', db='Chinook')
 
 try:
-    #Run a query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+    # Run a query
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                         Friends(Name char(20), age int, DOB datetime);""")
+
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
